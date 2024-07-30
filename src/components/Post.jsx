71f29de8f577cb2f42538a747372efdfb094fb1c
najmulhouda <najmulhouda6@gmail.com>
2024-07-30 23:12:02 +0600

    it's all about curfew

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./post.module.css";
+
+const Post = (props) => {
+  return (
+    <li className={classes.Post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </li>
+  );
+};
+
+export default Post;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Post from "./Post";
+import classes from "./postList.module.css";
 const PostList = () => {
-  return <div>PostList</div>;
+  return (
+    <ul className={classes.posts}>
+      <Post author="najmul" body="this is najmul " />
+      <Post author="houda" body="this is houda" />
+    </ul>
+  );
 };
 
 export default PostList;
