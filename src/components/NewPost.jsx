839708841f311cf10d4d062f2fd8b4e1048c6844
najmulhouda <najmulhouda6@gmail.com>
2024-08-09 22:20:49 +0600

    How I Made My Website Load in 0.364 Seconds

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import classes from "./NewPost.module.css";
 const NewPost = () => {
+  const changeBodyHander   
   return (
     <div>
       <form className={classes.form}>
-        <p className={classes.formLabel}>
-          <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} />
+        <p>
+          <label htmlFor="body">Text</label> <br />
+          <textarea id="body" required rows={3} onChange={}/>
         </p>
         <p>
-          <label htmlFor="name">Your name</label>
-          <textarea name="text" id="name" required />
+          <label htmlFor="name">Your name</label> <br />
+          <textarea name="text" id="name" required  />
         </p>
       </form>
-    </div>
   );
 };
 
 export default NewPost;
+
+// How I Made My Website Load in 0.364 Seconds
